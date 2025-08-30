b9f06e292245f3e3f0a3d514d77d455532f988e7
GalladeFan475 <smashingsanjay6@gmail.com>
2025-08-30 16:06:42 +0800

    new features added

---
+++ library/script.js
@@ -0,0 +1,81 @@
+const myLibrary = [];
+const libraryContainer = document.querySelector(".container");
+
+function Book(title, author, pages) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = false;
+  this.ID = crypto.randomUUID();
+}
+
+Book.prototype.toggleReadStatus = function () {
+  this.read = !this.read;
+};
+
+function addBookToLibrary(title, author, pages) {
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
+}
+
+function createBookElement(el, content) {
+  const element = document.createElement(el);
+  element.textContent = content;
+  return element;
+}
+
+function createBookCard() {
+  libraryContainer.textContent = "";
+
+  myLibrary.forEach((book) => {
+    // Create Card
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("class", "book-card");
+
+    // Add Title to Card
+    bookCard.appendChild(createBookElement("h3", `Book Title: ${book.title}`));
+
+    // Add Details to Card
+    const bookDetails = document.createElement("div");
+    bookCard.setAttribute("class", "book-details");
+
+    bookDetails.appendChild(createBookElement("h4", `Book ID: ${book.ID}`));
+    bookDetails.appendChild(
+      createBookElement("h4", `Book Author: ${book.author}`)
+    );
+    bookDetails.appendChild(
+      createBookElement("h4", `Book Pages: ${book.pages}`)
+    );
+    bookCard.appendChild(bookDetails);
+
+    // Create buttons
+    const bookButtons = document.createElement("div");
+    bookButtons.setAttribute("class", "book-btn");
+
+    bookButtons.appendChild(
+      createBookElement("button", book.isRead ? "Read" : "Unread").setAttribute(
+        "class",
+        book.isRead ? "book-read-btn toggle" : "book-unread-btn toggle"
+      )
+    );
+    bookButtons.appendChild(
+      createBookElement("button", "Delete Button").setAttribute(
+        "class",
+        "book-delete-btn"
+      )
+    );
+
+    // Give buttons function
+    document.querySelector(".toggle").addEventListener("click", (e) => {
+      book.toggleReadStatus();
+      e.target.textContent = book.isRead ? "Read" : "Unread";
+    });
+    document.querySelector(".book-delete-btn").addEventListener("click", () => {
+      const index = myLibrary.findIndex((b) => b.id === book.id);
+      myLibrary.splice(index, 1);
+    });
+  });
+}
